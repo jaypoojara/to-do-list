d094ffd051eb7772f7b799b0b6b88fbcfa9187bf
Jay Poojara <jay.poojara@gmail.com>
2024-12-02 15:35:07 +0530

    updated name of the table

--- migrations/20241202082452-add-due-date-to-tasks.js
+++ migrations/20241202082452-add-due-date-to-tasks.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('tasks', 'due_date', {
+    await queryInterface.addColumn('Tasks', 'due_date', {
       type: Sequelize.DATE,
       allowNull: true,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('tasks', 'due_date');
+    await queryInterface.removeColumn('Tasks', 'due_date');
   },
 };
