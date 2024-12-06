9429b31b50df43b9e4bb0750ad1bd3e887d99a31
Jay Poojara <jay.poojara@gmail.com>
2024-12-06 14:29:17 +0530

    refactored the code of app.js

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 import 'dotenv/config'
-import db from './models/index.js';
 import express from 'express';
 import * as path from 'node:path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
-import tasksRouter from './routes/tasks.js';
+import routers from "./routes/index.js";
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.disable('x-powered-by')
 
 app.use(cors());
 app.use(logger('dev'));
@@ -22,24 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tasks', tasksRouter);
-
-(async () => {
-  try {
-    await db.sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-})();
-
-// Handle non-existing routes
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'The requested resource was not found on this server.',
-  });
-});
+app.use('/',routers)
 
 export default app;
